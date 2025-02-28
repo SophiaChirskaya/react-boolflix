1a6b46193880a72946bde17b0678e67d999c1271
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-28 19:55:20 +0100

    [feat]Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="container">
+                <div className="navbar">
+                    <div>
+                        Logo
+                    </div>
+
+                </div>
+
+            </div>
+        </header>
+    )
+}
