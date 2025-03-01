46d3bc4c5aaf4b52e4946a0d32f89ef9082ea46f
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-03-01 16:52:29 +0100

    [feat]flagsMap

---
+++ src/libs/flags.js
@@ -0,0 +1,13 @@
+import itflag from '..assets/flags/it.jpg'
+import enflag from '..assets/flags/en.jpg'
+import frflag from '..assets/flags/fr.jpg'
+import deflag from '..assets/flags/de.jpg'
+
+const flagsMap = {
+    it: itflag,
+    en: enflag,
+    de: deflag,
+    fr: frflag,
+}
+
+export default flagsMap
