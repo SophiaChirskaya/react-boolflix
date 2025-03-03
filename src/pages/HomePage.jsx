d0f6ad8889f9944ee837853424edc1c5587f865b
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-03-03 12:54:56 +0100

    [feat]HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import ItemList from './../components/ItemList'
+
+export default function HomePage() {
+
+    return (
+        <>
+        <div>
+            <ItemList></ItemList>
+        </div>
+        </>
+    )
+}
